58be79c9a4673910a73c34dd5cd8262f6e9fc631
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-06-26 13:44:42 +0600

    updated helloBasicRouting.js

--- helloBasicRouting.js
+++ helloBasicRouting.js
@@ -1,15 +1,31 @@
 const http = require('http');
 
-function index (request, response){
-    response.writeHead(200);
-    response.end('Hello, world with basic routing');
-}
+// function index (request, response){
+//     response.writeHead(200);
+//     response.end('Hello, world with basic routing');
+// }
 
 http.createServer(function (request, response){
-    if(request.url === '/'){
-        return index(request, response);
+    
+    // if(request.url === '/'){
+    //     return index(request, response);
+    // }
+
+    if(request.url in routes){
+        return routes[request.url](request, response);
     }
 
     response.writeHead(404);
     response.end(http.STATUS_CODES[404]);
 }).listen(4201);
+
+var routes = {
+    '/' : function index(request, response){
+        response.writeHead(200);
+        response.end('Hello, world with basic routing');
+    },
+    '/about' : function about (request, response) {
+        response.writeHead(200);
+        response.end('This is About page from hello world with basic routing');
+    }
+}
